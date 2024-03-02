05b4f0ddaca6e8f8d98efaea3a334fe52b3fc380
ahmadnasser03 <129513327+ahmadnasser03@users.noreply.github.com>
2024-03-03 00:45:13 +0200

    fix

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ document.addEventListener("DOMContentLoaded", () => {
     fetchSounds().then(sounds => {
         const soundboard = document.getElementById("soundboard");
         sounds.forEach(sound => {
-            const soundDiv = document.createElement('div');
+            const soundDiv = document.createElement('button');
             soundDiv.classList.add("sound");
             soundDiv.id = sound.display
             soundDiv.innerText = sound.display;
-            soundDiv.addEventListener('click', () => {
+            soundDiv.onclick = function() {
                 const audio = new Audio('/assets/audios/' + sound.file + ".mp3"); 
                 audio.play().catch(error => console.error("Error playing the sound:", error));
-            });
+            };
             soundboard.appendChild(soundDiv);
         });
     })
